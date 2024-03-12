ebfd0637ad80e5c3572a5bb9b17298ba79dd05e4
edwintantawi <edwintantawi.dev@gmail.com>
2024-03-12 11:06:41 +0700

    feat: add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
+import { Button } from '~/components/ui/button';
+
 export default function RootPage() {
-  return <h1 className="text-center font-bold">edwintantawi.dev</h1>;
+  return (
+    <main className="px-3 md:px-4">
+      <header className="space-y-6 py-12">
+        <div className="flex flex-col items-center text-center">
+          <p className="mb-1 w-fit rounded-full border bg-slate-100 px-3 py-1 text-xs text-muted-foreground md:mb-2 md:text-sm">
+            👋 Hi, I&apos;m Edwin Tantawi
+          </p>
+          <h1 className="mb-1 text-4xl font-extrabold md:mb-2 md:text-5xl">
+            Web <span className="text-slate-500">Front-End</span> Engineer
+          </h1>
+          <p className="text-sm text-muted-foreground md:text-xl">
+            Crafting Engaging Web Experiences
+          </p>
+        </div>
+
+        <div className="mx-auto flex max-w-96 gap-2">
+          <Button size="lg" className="w-full">
+            Get In Touch
+          </Button>
+          <Button size="lg" variant="outline" className="w-full">
+            Resume
+          </Button>
+        </div>
+      </header>
+    </main>
+  );
 }
--- components/app-bar.tsx
+++ components/app-bar.tsx
@@ -3,7 +3,7 @@ import { Menu } from '~/components/menu';
 
 export function AppBar() {
   return (
-    <nav className="sticky top-0 flex items-center justify-between bg-background p-3 md:p-4 md:px-10 md:py-3 lg:px-20">
+    <nav className="sticky top-0 flex items-center justify-between border-b bg-background p-3 md:p-4">
       <div className="flex items-center gap-1 font-mono text-xs text-slate-500 md:gap-2 md:text-sm">
         <Logo className="size-8" />
         <span>/</span>
